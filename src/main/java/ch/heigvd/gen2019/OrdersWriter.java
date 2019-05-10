a9cabd6cee4ee064de05c72fea57d1f616b09c02
yoann0000 <47739072+yoann0000@users.noreply.github.com>
2019-05-10 15:37:32 +0200

    Extract method
    Applied recursively to getContents() in class OrdersWriter

--- src/main/java/ch/heigvd/gen2019/OrdersWriter.java
+++ src/main/java/ch/heigvd/gen2019/OrdersWriter.java
@@ -8,53 +8,74 @@ public class OrdersWriter {
     }
 
     public String getContents() {
-        StringBuffer sb = new StringBuffer("{\"orders\": [");
+        StringBuilder sb = new StringBuilder("{\"orders\": [");
+        printOrders(sb, orders);
+        return sb.append("]}").toString();
+    }
 
+    private void printOrders(StringBuilder sb, Orders orders) {
         for (int i = 0; i < orders.getOrdersCount(); i++) {
             Order order = orders.getOrder(i);
             sb.append("{");
-            sb.append("\"id\": ");
-            sb.append(order.getOrderId());
+            printOrderId(sb, order);
             sb.append(", ");
-            sb.append("\"products\": [");
-            for (int j = 0; j < order.getProductsCount(); j++) {
-                Product product = order.getProduct(j);
-
-                sb.append("{");
-                sb.append("\"code\": \"");
-                sb.append(product.getCode());
-                sb.append("\", ");
-                sb.append("\"color\": \"");
-                sb.append(getColorFor(product));
-                sb.append("\", ");
-
-                if (product.getSize() != Product.SIZE_NOT_APPLICABLE) {
-                    sb.append("\"size\": \"");
-                    sb.append(getSizeFor(product));
-                    sb.append("\", ");
-                }
-
-                sb.append("\"price\": ");
-                sb.append(product.getPrice());
-                sb.append(", ");
-                sb.append("\"currency\": \"");
-                sb.append(product.getCurrency());
-                sb.append("\"}, ");
-            }
-
-            if (order.getProductsCount() > 0) {
-                sb.delete(sb.length() - 2, sb.length());
-            }
-
-            sb.append("]");
+            printProducts(sb, order);
             sb.append("}, ");
         }
-
         if (orders.getOrdersCount() > 0) {
             sb.delete(sb.length() - 2, sb.length());
         }
+    }
 
-        return sb.append("]}").toString();
+    private void printProducts(StringBuilder sb, Order order) {
+        sb.append("\"products\": [");
+        for (int j = 0; j < order.getProductsCount(); j++) {
+            Product product = order.getProduct(j);
+            printProduct(sb, product);
+            sb.append(", ");
+        }
+
+        if (order.getProductsCount() > 0) {
+            sb.delete(sb.length() - 2, sb.length());
+        }
+        sb.append("]");
+    }
+
+    private void printOrderId(StringBuilder sb, Order order) {
+        sb.append("\"id\": ").append(order.getOrderId());
+    }
+
+    private void printProduct(StringBuilder sb, Product product) {
+        sb.append("{");
+        printProductCode(sb, product);
+        printProductColor(sb, product);
+
+        if (product.getSize() != Product.SIZE_NOT_APPLICABLE) {
+            printProductSize(sb, product);
+        }
+
+        printProductPrice(sb, product);
+        printProductCurrency(sb, product);
+    }
+
+    private void printProductCurrency(StringBuilder sb, Product product) {
+        sb.append("\"currency\": \"").append(product.getCurrency()).append("\"}");
+    }
+
+    private void printProductPrice(StringBuilder sb, Product product) {
+        sb.append("\"price\": ").append(product.getPrice()).append(", ");
+    }
+
+    private void printProductSize(StringBuilder sb, Product product) {
+        sb.append("\"size\": \"").append(getSizeFor(product)).append("\", ");
+    }
+
+    private void printProductColor(StringBuilder sb, Product product) {
+        sb.append("\"color\": \"").append(getColorFor(product)).append("\", ");
+    }
+
+    private void printProductCode(StringBuilder sb, Product product) {
+        sb.append("\"code\": \"").append(product.getCode()).append("\", ");
     }
 
     private String getSizeFor(Product product) {
